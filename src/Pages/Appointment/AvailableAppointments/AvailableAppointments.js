7e3ecaefa2ed059f42b68994eedf51b3d8fce744
mohon63 <mohonhossen63@gmail.com>
2023-04-04 11:09:52 +0600

    appoitment modal

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,37 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
+import BookingModal from '../BookingModal/BookingModal';
 
 const AvailableAppointments = ({ selectedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState([null])
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
-        <section className='mt-16'>
+        <section className='my-16'>
             <p className='text-xl text-secondary text-center font-bold'>Available Services on {format(selectedDate, 'PP')}</p>
+            <p className='text-xl text-center font-bold' >Please select a service.</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+            </div>
+            {
+                treatment &&
+                <BookingModal
+                    treatment={treatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
